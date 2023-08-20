ecf3c4b553f5b3064e341e9440321971fee9258e
Vaughn Kottler <vaughnkottler@gmail.com>
2023-08-20 01:56:39 -0500

    Add running tests to CI

--- tests/commands/test_gen.py
+++ tests/commands/test_gen.py
@@ -5,6 +5,10 @@ Test the 'commands.gen' module.
 # built-in
 from subprocess import run
 from sys import executable, platform
+from typing import Any
+
+# third-party
+from vcorelib.io import ARBITER
 
 # module under test
 from ifgen import PKG_NAME
@@ -17,10 +21,19 @@ from tests.resources import clean_scenario
 def test_ifgen_command_basic():
     """Test the 'gen' command."""
 
-    path = str(clean_scenario("sample"))
-    assert ifgen_main([PKG_NAME, "gen", "-r", path]) == 0
+    path = clean_scenario("sample")
+    assert ifgen_main([PKG_NAME, "gen", "-r", str(path)]) == 0
 
     # Attempt to build and run generated tests.
     if platform == "linux":
-        run([executable, "-m", "yambs", "-C", path, "native"], check=True)
-        run(["ninja", "-C", path, "format-check"], check=True)
+        run([executable, "-m", "yambs", "-C", str(path), "native"], check=True)
+        run(["ninja", "-C", str(path), "format-check"], check=True)
+
+        # Run test apps.
+        data: dict[str, Any] = ARBITER.decode(
+            path.joinpath("ninja", "apps.json"), require_success=True
+        ).data
+        tests = data["tests"]
+        for name, data in data["all"].items():
+            if name in tests:
+                run([data["variants"]["debug"]], check=True)
