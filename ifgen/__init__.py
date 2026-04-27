7320c70f9258ad99e68aacc798be32bdf6d1ec34
v <vaughn@libre-embedded.com>
2026-04-27 17:08:15 -0500

    4.8.1 - write-only field setting (#119)

--- ifgen/__init__.py
+++ ifgen/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.2.4
-# hash=8a1f34623714ecc13fb0fb295fdcb985
+# hash=58522fe09daad2768a466de6db6abf78
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "An interface generator for distributed computing."
 PKG_NAME = "ifgen"
-VERSION = "4.8.0"
+VERSION = "4.8.1"
--- ifgen/struct/methods/fields/setter.py
+++ ifgen/struct/methods/fields/setter.py
@@ -139,10 +139,13 @@ def bit_field_set_method(
             if "index" in inner:
                 rhs += "[index]"
 
-            writer.write(f"{parent['type']} curr = {rhs};")
+            if field["read"]:
+                writer.write(f"{parent['type']} curr = {rhs};")
 
-            with writer.padding():
-                for line in bit_field_set_lines(task, field):
-                    writer.write(line)
+                with writer.padding():
+                    for line in bit_field_set_lines(task, field):
+                        writer.write(line)
 
-            writer.write(f"{rhs} = curr;")
+                writer.write(f"{rhs} = curr;")
+            else:
+                writer.write(f"{rhs} = value;")
